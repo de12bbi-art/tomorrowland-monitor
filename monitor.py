4f840aaed373b53aceb3a4015c5dc14f6ac458a2
de12bbi-art <de12bbi@gmail.com>
2026-06-22 09:49:00 +0200

    Update monitor.py
    TEST

--- monitor.py
+++ monitor.py
@@ -6,7 +6,7 @@ from playwright.sync_api import sync_playwright
 
 URL = "https://www.viagogo.it/Biglietti-Festivals/Festival-Internazionali/Tomorrowland-Biglietti/E-160250859?quantity=1"
 
-THRESHOLD = 500
+THRESHOLD = 700
 
 
 BOT_TOKEN = os.environ["TELEGRAM_TOKEN"]
